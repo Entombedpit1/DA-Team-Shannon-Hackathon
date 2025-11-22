79be98383cfb73b664be60acbe062732b1927645
Hans <hansgggao@gmail.com>
2025-11-22 13:38:23 -0800

    ai work

--- artificial stupidity/mcts.gd
+++ artificial stupidity/mcts.gd
@@ -21,7 +21,9 @@ func _ready():
 	sample_gamestate.ranks_on_board = []
 	sample_gamestate.is_terminal = false
 	# First card is selected as Trump suit and removed from draw pile
-	sample_gamestate.trump_suit = sample_gamestate.draw_pile.pop_back().Suit
+	var trump_card = sample_gamestate.draw_pile.pop_back()
+	if trump_card != null:
+		sample_gamestate.trump_suit = trump_card.Suit
 
 	print("Player health: ", sample_gamestate.player_curr_health)
 	print("Trump suit: ", sample_gamestate.trump_suit)
@@ -34,6 +36,9 @@ func _ready():
 	sample_gamestate = attack(sample_gamestate, sample_gamestate.player_hand[0])
 	print("##########################################")
 	print(mountain_cargo_tree_search(sample_gamestate, 1000))
+	print("##########################################")
+	print("##########################################")
+	print(mountain_cargo_tree_search(sample_gamestate, 10000))
 	print("##########################################")
 
 class GameState:
@@ -90,45 +95,77 @@ class GameState:
 		print("IS computer attacking?? ", self.computer_is_attacking)
 		
 
+func deep_gamestate_copy(node:GameState) -> GameState:
+	var new_gamestate:GameState = GameState.new()
+	new_gamestate.player_max_health = node.player_max_health
+	new_gamestate.player_curr_health = node.player_curr_health
+	new_gamestate.computer_is_attacking = node.computer_is_attacking
+	new_gamestate.computer_max_health = node.computer_max_health
+	new_gamestate.computer_curr_health = node.computer_curr_health
+	new_gamestate.draw_pile = node.draw_pile.duplicate()
+	new_gamestate.computer_hand = node.computer_hand.duplicate()
+	new_gamestate.player_hand = node.player_hand.duplicate()
+	new_gamestate.table_arr = node.table_arr.duplicate()
+	new_gamestate.discard_pile = node.discard_pile.duplicate()
+	new_gamestate.active_attacks = node.active_attacks.duplicate()
+	new_gamestate.ranks_on_board = node.ranks_on_board.duplicate()
+	new_gamestate.trump_suit = node.trump_suit
+	new_gamestate.is_terminal = node.is_terminal
+	new_gamestate.result_of_computer = node.result_of_computer
+	return new_gamestate
+
 func get_legal_actions(game_state:GameState) -> Array[Callable]:
 	var call_arr:Array[Callable]
 	if (!game_state.ranks_on_board.is_empty()):
-		call_arr.push_back(end_turn.bind(game_state))
+		call_arr.push_back(end_turn.bind(deep_gamestate_copy(game_state)))
 	else:
 		if (game_state.computer_is_attacking):
 			for card in game_state.computer_hand:
-				call_arr.push_back(attack.bind(game_state, card))
+				if card == null:
+					continue
+				call_arr.push_back(attack.bind(deep_gamestate_copy(game_state), card))
 		else:
 			for card in game_state.player_hand:
-				call_arr.push_back(attack.bind(game_state, card))
+				if card == null:
+					continue
+				call_arr.push_back(attack.bind(deep_gamestate_copy(game_state), card))
 	if (game_state.computer_is_attacking):
 		for card in game_state.computer_hand:
-			print(game_state.computer_hand.size(), game_state.computer_hand[game_state.computer_hand.size() - 1])
+			if card == null:
+				continue
 			if (game_state.ranks_on_board.has(card.Rank)):
-				call_arr.push_back(attack.bind(game_state, card))
+				call_arr.push_back(attack.bind(deep_gamestate_copy(game_state), card))
 		for atk_card in game_state.active_attacks:
+			if atk_card == null:
+				continue
 			for card in game_state.player_hand:
+				if card == null:
+					continue
 				if (card.Suit == atk_card.Suit):
 					if (card.Rank > atk_card.Rank):
-						call_arr.push_back(defend.bind(game_state, atk_card, card))
+						call_arr.push_back(defend.bind(deep_gamestate_copy(game_state), atk_card, card))
 				elif (card.Suit == game_state.trump_suit):
-					call_arr.push_back(defend.bind(game_state, atk_card, card))
+					call_arr.push_back(defend.bind(deep_gamestate_copy(game_state), atk_card, card))
 					
 	else:
-		# computer is attacking
-		print("size: ",game_state.computer_hand.size())
-		for card in game_state.computer_hand:
-			print(card.Rank, card.Suit)
+		# player is attacking, computer is defending
+		for card in game_state.player_hand:
+			if card == null:
+				continue
 			if (game_state.ranks_on_board.has(card.Rank)):
-				call_arr.push_back(attack.bind(game_state, card))
-		# player is defending
+				call_arr.push_back(attack.bind(deep_gamestate_copy(game_state), card))
+		# computer is defending
 		for atk_card in game_state.active_attacks:
+			if atk_card == null:
+				continue
 			for card in game_state.computer_hand:
+				if card == null:
+					continue
 				if (card.Suit == atk_card.Suit):
 					if (card.Rank > atk_card.Rank):
-						call_arr.push_back(defend.bind(game_state, atk_card, card))
+						call_arr.push_back(defend.bind(deep_gamestate_copy(game_state), atk_card, card))
 				elif (card.Suit == game_state.trump_suit):
-					call_arr.push_back(defend.bind(game_state, atk_card, card))
+					call_arr.push_back(defend.bind(deep_gamestate_copy(game_state), atk_card, card))
 	return call_arr
 
 class MctsNode:
@@ -151,17 +188,25 @@ class MctsNode:
 		return ret_node
 		
 	func upper_conf_bound() -> float:
+		if self.visit_count == 0:
+			return INF
+		if self.parent_node == null:
+			return 1.0 * self.total_wins / self.visit_count
 		return 1.0 * self.total_wins/self.visit_count + sqrt(2)*sqrt(log(self.parent_node.visit_count)/self.visit_count)
 	
 	func find_highest_avg_reward_action() -> Callable:
+		if self.children.is_empty():
+			return Callable()
 		var highest_child:MctsNode = self.children[0]
-		var highest_avg_reward:float = 1.0*highest_child.total_wins/highest_child.visit_count
+		# Normalize reward from [-1, 1] to [0, 1] range: (reward + 1) / 2
+		var highest_avg_reward:float = (1.0*highest_child.total_wins/max(highest_child.visit_count, 1) + 1.0) / 2.0
 		for child in self.children:
-			var curr_avg:float = (1.0*child.total_wins/child.visit_count)
+			# Normalize reward from [-1, 1] to [0, 1] range
+			var curr_avg:float = (1.0*child.total_wins/max(child.visit_count, 1) + 1.0) / 2.0
 			if (curr_avg > highest_avg_reward):
 				highest_child = child
 				highest_avg_reward = curr_avg
-		print("highest_avg_reward", highest_avg_reward)
+		print (highest_avg_reward)
 		return highest_child.action_parent_took
 
 func navigate_to_leaf(node:MctsNode) -> MctsNode:
@@ -174,34 +219,36 @@ func mountain_cargo_tree_search(root_state:GameState, iteration_limit:int) -> Ca
 	var root_node:MctsNode = MctsNode.new()
 	root_node.game_state = root_state
 	root_node.parent_node = null
-	root_node.available_actions = get_legal_actions(root_node.game_state)
-	print("FOR THIS SCENARIO")
-	#root_node.game_state.print_game_state()
-	print("available acitons:::::::")
-	print(root_node.available_actions)
+	root_node.available_actions = get_legal_actions(deep_gamestate_copy(root_node.game_state))
+	
+	
 	var i:int = 0
 	while (i < iteration_limit):
-		var leaf_node:MctsNode = MctsNode.new()
-		leaf_node.parent_node = root_node
-		var available_actions:Array[Callable]
-		var action_parent_took:Callable
-		var game_state:GameState
+		var leaf_node:MctsNode = navigate_to_leaf(root_node)
 		var score_result:int
-		while (leaf_node.available_actions.is_empty()):
-			leaf_node = leaf_node.max_ucb_child_of_node()
-		if (!leaf_node.game_state.is_terminal):
+		if (!leaf_node.game_state.is_terminal && !leaf_node.available_actions.is_empty()):
 			leaf_node = mcts_expand(leaf_node)
 			score_result = simulate_node_until_end(leaf_node)
 		else:
 			score_result = leaf_node.game_state.result_of_computer
+		
+		# Backpropagate: negate reward when going through opponent nodes
+		# Reward is always from computer's perspective, so we negate at opponent nodes
+		var current_reward:int = score_result
+		var is_computer_turn:bool = leaf_node.game_state.computer_is_attacking
 		leaf_node.visit_count += 1
-		leaf_node.total_wins += score_result
+		leaf_node.total_wins += current_reward
 		while (leaf_node.parent_node != null):
 			leaf_node = leaf_node.parent_node
+			# Negate reward when parent is opponent's turn (opponent wants to minimize computer's reward)
+			is_computer_turn = leaf_node.game_state.computer_is_attacking
+			if !is_computer_turn:
+				current_reward = -current_reward
 			leaf_node.visit_count += 1
-			leaf_node.total_wins += score_result
+			leaf_node.total_wins += current_reward
 		i += 1
-	return new_node.find_highest_avg_reward_action()
+	
+	return root_node.find_highest_avg_reward_action()
 
 func mcts_expand(node:MctsNode) -> MctsNode:
 	
@@ -217,23 +264,24 @@ func mcts_expand(node:MctsNode) -> MctsNode:
 
 
 func simulate_node_until_end(node:MctsNode) -> int:
-	var game_state:GameState = node.game_state
-	var sim_state:GameState = node.game_state
+	var sim_state:GameState = deep_gamestate_copy(node.game_state)
 	while (sim_state.is_terminal == false):
-		print("Available actions:-----------")
-		print(get_legal_actions(sim_state))
-		print("drawpilesize: ",sim_state.draw_pile.size())
-		sim_state = get_legal_actions(sim_state).pick_random().call()
+		var legal_actions = get_legal_actions(sim_state)
+		if legal_actions.is_empty():
+			# No legal actions, end turn or mark as terminal
+			sim_state.is_terminal = true
+			sim_state.result_of_computer = 0
+			break
+		sim_state = legal_actions.pick_random().call()
 		
-	print("Terminated")
-	#sim_state.print_game_state()
-	print(sim_state.result_of_computer)
 	return sim_state.result_of_computer
 
 
 
 
 func attack(game_state:GameState, card:CardResource) -> GameState:
+	if card == null:
+		return game_state
 	if game_state.table_arr.is_empty() || game_state.ranks_on_board.has(card.Rank):
 		game_state.ranks_on_board.append(card.Rank)
 		game_state.active_attacks.append(card)
@@ -243,10 +291,11 @@ func attack(game_state:GameState, card:CardResource) -> GameState:
 			game_state.player_hand.erase(card)
 		return game_state
 	else:
-		print("Not allowed! rank not on table")
 		return game_state
 
 func defend(game_state:GameState, attacking_card:CardResource, defending_card:CardResource) -> GameState:
+	if attacking_card == null || defending_card == null:
+		return game_state
 	if (defending_card.Suit == game_state.trump_suit || defending_card.Suit == attacking_card.Suit):
 		if (defending_card.Rank > attacking_card.Rank || attacking_card.Suit != game_state.trump_suit):
 			# REMEMBER TO IMPLEMENT GRAPHICS
@@ -259,29 +308,21 @@ func defend(game_state:GameState, attacking_card:CardResource, defending_card:Ca
 				game_state.player_hand.erase(defending_card)
 			else:
 				game_state.computer_hand.erase(defending_card)
-			print("successfully defended")
 			return game_state
-	print("not allowed! defend better you bozo")
 	return game_state
 
 func end_turn(game_state:GameState) -> GameState:
-	print("END TURN, DAMAGE CALC")
-	#game_state.print_game_state()
 	if (game_state.active_attacks.is_empty()):
 		# defender is successful
-		print("Defender is defend")
+		pass
 	else:
 		for card in game_state.active_attacks:
 			if (game_state.computer_is_attacking):
 				game_state.player_curr_health -= card.Damage
-				print("Computer dealt ", card.Damage, " damage")
 			else:
 				game_state.computer_curr_health -= card.Damage
-				print("Player dealt ", card.Damage, " damage")
 			game_state.discard_pile.append(card)
 		game_state.active_attacks.clear()
-	print("Player has ", game_state.player_curr_health, " health")
-	print("Computer has ", game_state.computer_curr_health, " health")
 	for card in game_state.table_arr:
 		game_state.discard_pile.append(card)
 	game_state.table_arr.clear() # clear inactive cards (not actively attacking)
@@ -304,9 +345,6 @@ func end_turn(game_state:GameState) -> GameState:
 		game_state.result_of_computer = -1;
 		game_state.is_terminal = true;
 	game_state.computer_is_attacking = !game_state.computer_is_attacking
-	print("ENDING TURN ~~~~~~~")
-	#game_state.print_game_state()
-	print("TURN ENDED ~~~~~~~")
 	return game_state
 
 #func simulate_next_step_rand(game_state:GameState) -> GameState:
